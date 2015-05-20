b343804207de3de393944adf10ed8dd4aae981fe
andrewchaa <andrew.chaa@huddle.com>
2015-05-20 18:43:02 +0100

    Linked List

---
+++ LearningAlgorithms/LinkedListTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LearningAlgorithms
+{
+    [TestFixture]
+    public class LinkedListTests
+    {
+        private ListElement<int> _element1;
+        private ListElement<int> _element2;
+
+
+        [Test]
+        public void It_should_point_to_the_next_element()
+        {
+            //arrange
+            _element1 = new ListElement<int>(1);
+            _element2 = new ListElement<int>(2);
+
+            //act
+            _element1.SetNext(_element2);
+
+            //assert
+            Assert.That(_element1.Next, Is.EqualTo(_element2));
+        }
+
+        [Test]
+        public void It_should_contain_a_value()
+        {
+            //act
+            _element1 = new ListElement<int>(1);
+            _element2 = new ListElement<int>(2);
+
+            //assert
+            Assert.That(_element1.Value, Is.EqualTo(1));
+            Assert.That(_element2.Value, Is.EqualTo(2));
+        }
+    }
+}
---
+++ LearningAlgorithms/ListElement.cs
@@ -0,0 +1,18 @@
+namespace LearningAlgorithms
+{
+    public class ListElement<T>
+    {
+        public ListElement<T> Next { get; private set; }
+        public T Value { get; private set; }
+
+        public ListElement(T value)
+        {
+            Value = value;
+        }
+
+        public void SetNext(ListElement<T> element)
+        {
+            Next = element;
+        }
+    }
+}
