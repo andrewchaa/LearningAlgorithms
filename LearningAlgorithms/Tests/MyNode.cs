1b9c8c3b91d7092ed6f2910757f9c378ca6c7e4a
Andrew <andrew.chaa@yahoo.co.uk>
2015-05-23 18:51:56 +0100

    added node

---
+++ LearningAlgorithms/Tests/MyNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LearningAlgorithms.Tests
+{
+    public class MyNode
+    {
+        public int Value { get; private set; }
+        public IList<MyNode> Nodes { get; private set; }
+
+        public MyNode(int value)
+        {
+            Value = value;
+            Nodes = new List<MyNode>();
+        }
+
+        public void Add(MyNode node)
+        {
+            Nodes.Add(node);
+        }
+    }
+}
---
+++ LearningAlgorithms/Tests/TreeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LearningAlgorithms.Tests
+{
+    [TestFixture]
+    public class TreeTests
+    {
+        [Test]
+        public void Node_should_have_a_value()
+        {
+            // arrange
+            // act
+            var node = new MyNode(1);
+
+            // assert
+            Assert.That(node.Value, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Node_has_children()
+        {
+            // arrange
+            var node = new MyNode(1);
+            var childNode = new MyNode(2);
+
+            // act
+            node.Add(childNode);
+
+            // assert
+            Assert.That(node.Nodes.Contains(childNode), Is.True);
+        }
+    }
+}
